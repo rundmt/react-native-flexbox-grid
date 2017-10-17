c4948709c84c53a61b5b06d0fefb7e5c3ee40650
Anush <anush@techstream.org>
2017-10-17 16:06:26 +0530

    Changed react.propType to prop-types

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
 import {screenSize} from '../lib/ScreenSize';
 import {isHidden, getComponentWidth, getComponentOffset} from '../lib/helpers';
 import {View} from 'react-native';
