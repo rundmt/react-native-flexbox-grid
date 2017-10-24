da01798f8ae95f75bcb9f117d27a9f4be12e5f4c
Anush <anushbmx@gmail.com>
2017-10-24 22:52:54 +0530

    Changed react.propType to prop-types (#17)
    * Changed react.propType to prop-types
    
    * Fixed invalid json

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
 import {screenSize} from '../lib/ScreenSize';
 import {isHidden} from '../lib/helpers';
 import {View} from 'react-native';
